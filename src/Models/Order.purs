567b36c2c46bab3fa01bd601759919013d60f1b2
wvanheemstra <willem@vanheemstrasystems.com>
2018-03-17 21:34:03 +0100

    Add Model Order

---
+++ src/Models/Order.purs
@@ -0,0 +1,26 @@
+module Model.Order where
+
+import Prelude (class Show, Unit)
+
+import Data.Argonaut.Encode (class EncodeJson)
+import Data.Argonaut.Decode (class DecodeJson)
+import Data.Argonaut.Encode.Generic (gEncodeJson)
+import Data.Argonaut.Decode.Generic (gDecodeJson)
+import Data.Generic (class Generic, gShow)
+import Data.Endpoint (Endpoint(Endpoint))
+import Data.HTTP.Method (Method(GET))
+
+data Order = Order { productId :: Int
+                   , quantity :: Int }
+
+derive instance genericOrder :: Generic Order
+
+instance encodeJsonOrder :: EncodeJson Order where
+  encodeJson = gEncodeJson
+instance decodeJsonOrder :: DecodeJson Order where
+  decodeJson = gDecodeJson
+instance showOrder :: Show Order where
+  show = gShow
+
+getOrdersEndpoint :: Endpoint Int Unit (Array Order)
+getOrdersEndpoint = Endpoint {url: "/getorders", method: GET}

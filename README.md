3fb9b5eb7d0508f3ba8a2e6e1f40578a169f450b
wvanheemstra <willem@vanheemstrasystems.com>
2018-03-27 22:26:14 +0200

    Update

--- README.md
+++ README.md
@@ -25,6 +25,27 @@ start .\dist\index.html
 To run the project (currently up-to-date for Purescript 0.11.7):
 
 ```
+# Follow the instructions how to install Node-Gyp on 
+# https://github.com/nodejs/node-gyp
+# as administrator
+> npm install --global --production windows-build-tools
+
+# If you have NOT installed Node-Gyp as described above, then
+# Install Python 2.* 
+# and add 'python2' in the environment variable PATH
+
+# If you have NOT installed Node-Gyp as described above, then
+# Install Windows SDK
+# See https://developer.microsoft.com/en-us/windows/downloads/sdk-archive 
+# as it is required by Visual Studio 2015, when installing node-sass, 
+# and not provided by default
+
+# Install node-sass@4.5.3
+# It is now added to package.json
+
+# Update npm to a version >= 5.7.0
+> npm -g install npm
+
 # Install dependencies
 > bower install
 > npm install
@@ -79,3 +100,19 @@ References:
 And accompanying example at [https://github.com/FrigoEU/purescript-endpoints-express-example](https://github.com/FrigoEU/purescript-endpoints-express-example)
 
 "Halogen Menu" at [https://github.com/slamdata/purescript-halogen-menu](https://github.com/slamdata/purescript-halogen-menu)
+
+What to do when the Tessel 2 is not detected.
+
+We got our windows 7 and windows 10 laptops detecting the Tessel 2 and running blinkie.
+Here's what we did:
+
+1) Install LTS v4.4.4 from http://nodejs.org
+2) Get the zip file https://gist.github.com/tcr/992978a5dbe5bff2e18f495c5c0973c3
+3) Run node driver-clean.js
+4) Plug in Tessel 2
+5) Get Zadig - http://zadig.akeo.ie/ and run it, which should show the Tessel 2; simply click the install WinUSB button w/o changing anything - If you go to Device Manager, you should now notice that the three Tessel devices which previously had an ❗️ are now ok w/ the WinUSB driver
+6) Run cmd as Administrator
+7) Run npm i -g t2-cli
+8) Run t2 list which should show the USB connection
+9) Do the rest... http://tessel.github.io/t2-start/
+10) Rejoice 
--- server_tessel2.js
+++ server_tessel2.js
@@ -1,86 +1,83 @@
-  // These two dependencies remain the same
-  var tessel = require('tessel');
-  var http = require('http');
+// These two dependencies remain the same
+var tessel = require('tessel');
+var http = require('http');
 
-  // Require two other core Node.js modules
-  var fs = require('fs');
-  var url = require('url');
+// Require two other core Node.js modules
+var fs = require('fs');
+var url = require('url');
 
-  var server = http.createServer(function (request, response) {
-    // Break up the url into easier-to-use parts
-    var urlParts = url.parse(request.url, true);
+var server = http.createServer(function (request, response) {
+  // Break up the url into easier-to-use parts
+  var urlParts = url.parse(request.url, true);
 
-    // Create a regular expression to match requests to toggle LEDs
-    var ledRegex = /leds/;
+  // Create a regular expression to match requests to toggle LEDs
+  var ledRegex = /leds/;
 
-    if (urlParts.pathname.match(ledRegex)) {
-      console.log('urlParts: ', urlParts);
+  if (urlParts.pathname.match(ledRegex)) {
+    console.log('urlParts: ', urlParts);
+    // If there is a request containing the string 'leds' call a function, toggleLED
+    toggleLED(urlParts.pathname, request, response); // undefined function, should be in client.js
+  } else {
+    // All other request will call a function, showIndex
+    showIndex(urlParts.pathname, request, response);
+  }
+});
 
-      // If there is a request containing the string 'leds' call a function, toggleLED
-      toggleLED(urlParts.pathname, request, response);
+// Stays the same
+server.listen(8080);
+
+// Stays the same
+console.log('Server running at http://192.168.1.101:8080/');
+
+// Respond to the request with our index.html page
+function showIndex (url, request, response) {
+  // Create a response header telling the browser to expect html
+  response.writeHead(200, {"Content-Type": "text/html"});
+
+  // Use fs to read in index.html
+  fs.readFile(__dirname + '/dist/index.html', function (err, content) {
+    // If there was an error, throw to stop code execution
+    if (err) {
+      throw err;
+    }
+    // Serve the content of index.html read in by fs.readFile
+    response.end(content);
+  });
+}
+
+// Toggle the led specified in the url and respond with its state
+function toggleLED (url, request, response) {
+
+  console.log("URL: ", url, ", REQUEST: ", request);
+
+  // Create a regular expression to find the number at the end of the url
+  var indexRegex = /(\d)$/;
+
+  // Capture the number, returns an array
+  var result = indexRegex.exec(url);
+
+  // Grab the captured result from the array
+  var index = result[1];
+
+  // Use the index to refence the correct LED
+  var led = tessel.led[index];
+
+  // Toggle the state of the led and call the callback after that's done
+  led.toggle(function (err) {
+    if (err) {
+      // Log the error, send back a 500 (internal server error) response to the client
+      console.log(err);
+      response.writeHead(500, {"Content-Type": "application/json"});
+      console.log("RESPONSE: ", response);
+
+      response.end(JSON.stringify({error: err}));
     } else {
-      // All other request will call a function, showIndex
-      showIndex(urlParts.pathname, request, response);
+      // The led was successfully toggled, respond with the state of the toggled led using led.isOn
+      response.writeHead(200, {"Content-Type": "application/json"});
+
+      console.log("RESPONSE: ", response);
+
+      response.end(JSON.stringify({on: led.isOn}));
     }
   });
-
-  // Stays the same
-  server.listen(8080);
-
-  // Stays the same
-  console.log('Server running at http://192.168.1.101:8080/');
-
-  // Respond to the request with our index.html page
-  function showIndex (url, request, response) {
-    // Create a response header telling the browser to expect html
-    response.writeHead(200, {"Content-Type": "text/html"});
-
-    // Use fs to read in index.html
-    fs.readFile(__dirname + '/dist/index.html', function (err, content) {
-      // If there was an error, throw to stop code execution
-      if (err) {
-        throw err;
-      }
-
-      // Serve the content of index.html read in by fs.readFile
-      response.end(content);
-    });
-  }
-
-  // Toggle the led specified in the url and respond with its state
-  function toggleLED (url, request, response) {
-
-    console.log("URL: ", url, ", REQUEST: ", request);
-
-    // Create a regular expression to find the number at the end of the url
-    var indexRegex = /(\d)$/;
-
-    // Capture the number, returns an array
-    var result = indexRegex.exec(url);
-
-    // Grab the captured result from the array
-    var index = result[1];
-
-    // Use the index to refence the correct LED
-    var led = tessel.led[index];
-
-    // Toggle the state of the led and call the callback after that's done
-    led.toggle(function (err) {
-      if (err) {
-        // Log the error, send back a 500 (internal server error) response to the client
-        console.log(err);
-        response.writeHead(500, {"Content-Type": "application/json"});
-
-        console.log("RESPONSE: ", response);
-
-        response.end(JSON.stringify({error: err}));
-      } else {
-        // The led was successfully toggled, respond with the state of the toggled led using led.isOn
-        response.writeHead(200, {"Content-Type": "application/json"});
-
-        console.log("RESPONSE: ", response);
-
-        response.end(JSON.stringify({on: led.isOn}));
-      }
-    });
-  }
+}
